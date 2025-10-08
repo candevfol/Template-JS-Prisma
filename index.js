3c196117905d1cb74ebe80cfbe9b8061c50b5cdc
Aum <candevfol@gmail.com>
2025-10-08 15:20:07 +0530

    updated:readme

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ dotenv.config();
 
 import cors from "cors";
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server running on port ${process.env.PORT}`);
